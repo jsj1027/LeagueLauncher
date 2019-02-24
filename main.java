f2ba66c4ab0e71b493b08b3afb3f6459e931e154
Javis Johnson <jsj1027@g.rit.edu>
2019-02-24 15:07:56 -0500

    New java file, switching program to using java

---
+++ main.java
@@ -0,0 +1,24 @@
+public class LeagueLauncher{
+
+    private boolean leagueOnline = false;
+
+    public boolean getLeagueOnline() {
+        return leagueOnline;
+    }
+
+    public void setLeagueOnline(boolean leagueOnlineSetting){
+        leagueOnline = leagueOnlineSetting;
+        return;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    public void checkSystemProcesses(){
+        return;
+    }
+
+    public void readProcesses(){
+        return;
+    }
+}
